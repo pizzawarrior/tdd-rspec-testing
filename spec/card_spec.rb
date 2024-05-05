daf6b8164bad311d54864336db35384e1ce00d94
pizzawarrior <i.norstad@gmail.com>
2024-05-04 17:51:54 -0700

    build simple test, then simple class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,19 @@
-RSpec.describe 'Card' do
+# frozen_string_literal: true
 
+class Card
+  attr_reader :type, :color, :suit
+
+  def initialize(type, color, suit)
+    @type = type
+    @color = color
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  it 'has a type' do
+    card = Card.new('Ace of Spades', 'black', 'spades')
+    expect(card.type).to eq('Ace of Spades')
+    print card.suit
+  end
 end
