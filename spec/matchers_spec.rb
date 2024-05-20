8410dc0ad54552c7de98d966a95f7e5e1aeaf713
pizzawarrior <i.norstad@gmail.com>
2024-05-19 17:35:07 -0700

    equality matchers

--- spec/matchers_spec.rb
+++ spec/matchers_spec.rb
@@ -1,5 +1,5 @@
-# No to method
 RSpec.describe 'Exploring different matchers' do
+  # No to method
   context 'given the not_to method' do
     it 'checks that two values do not match' do
       expect(Array).not_to be_instance_of(Hash)
@@ -23,15 +23,29 @@ RSpec.describe 'Exploring different matchers' do
       expect(a).not_to eql(b)
     end
   end
+
   context 'equal and be matcher' do
     let(:c) { [1, 2, 3] }
     let(:d) { [1, 2, 3] }
     let(:e) { c }
-
+    # equal and be are the same method
     it 'test for sameness' do
       expect(e).not_to be(d)
       expect(c).not_to be(d)
       expect(c).to equal(e)
+      expect(c).to eql(e)
+      expect(c).to eq(e)
     end
+  end
+
+  context 'comparison matchers' do
+    it 'allows for comparison with built in ruby operators' do
+      expect(10).to be > 9.9999
+      expect(99).to be < 99.1
+      expect(1).to be >= -1
+    end
+  end
+  describe 100 do
+    it { is_expected.to be <= 100 }
   end
 end
