920d1221c9e498c7f0a03b90d8280f2cf5006d7c
pizzawarrior <i.norstad@gmail.com>
2024-05-20 14:07:00 -0700

    change matchers

--- spec/matchers_spec.rb
+++ spec/matchers_spec.rb
@@ -92,4 +92,11 @@ RSpec.describe 'Exploring different matchers' do
       expect(my_hash[:b]).to be_nil
     end
   end
+
+  context 'change matchers' do
+    subject { [1, 2, 3] }
+    it 'checks that a method changes object state' do
+      expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+    end
+  end
 end
