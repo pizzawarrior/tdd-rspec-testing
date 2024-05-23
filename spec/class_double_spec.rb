07331a53787a458af9acf4fa2e707eb1c7fdd4f0
pizzawarrior <i.norstad@gmail.com>
2024-05-23 14:06:39 -0700

    how to use a class double

---
+++ spec/class_double_spec.rb
@@ -0,0 +1,25 @@
+# class API
+#   def self.call_api
+#     # integration logic to make API call
+#   end
+# end
+
+class APIresponse
+  attr_reader :content
+
+  def success
+    @content = API.call_api
+  end
+end
+
+RSpec.describe APIresponse do
+  it 'can only implement class methods that are defined on the class' do
+    success_response = class_double('API',
+                                    call_api: { data: { cust_id: '897ucdb',
+                                                        invoice_id: '4947nmmcmc78' } }).as_stubbed_const
+    expect(success_response).to receive(:call_api)
+    subject.success
+    expect(subject.content).to eq({ data: { cust_id: '897ucdb',
+                                            invoice_id: '4947nmmcmc78' } })
+  end
+end
