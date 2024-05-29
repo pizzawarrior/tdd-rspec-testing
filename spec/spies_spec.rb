42d5c8954dd25bb679d03caf57ec1d1c61cadccb
pizzawarrior <i.norstad@gmail.com>
2024-05-29 12:22:03 -0700

    more spies

--- spec/spies_spec.rb
+++ spec/spies_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe 'spies' do
     expect(customer).to have_received(:buy_product)
     expect(customer).not_to have_received(:overcharge)
   end
+  it 'retains the same functionality of a normal double' do
+    customer.buy_product
+    customer.buy_product
+    customer.buy_product('new_contract')
+    expect(customer).to have_received(:buy_product).exactly(3).times
+    expect(customer).to have_received(:buy_product).at_least(2).times
+    expect(customer).to have_received(:buy_product).with('new_contract')
+    expect(customer).to have_received(:buy_product).once.with('new_contract')
+  end
 end
